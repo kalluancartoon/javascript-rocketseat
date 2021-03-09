a5b6e44d079a90d0d47e75c1274746869b183a30
Kalluan Cartoon <kalluanf@gmail.com>
2021-03-09 19:01:13 -0300

    Exerciciojs

---
+++ exercicio.js
@@ -0,0 +1,72 @@
+/* ### Transformar notas escolares 
+
+Crie um algoritimo que transforme as notas do sitema numérico para sistema de notas em caracteres tipo A B C
+
+ *  de 90 para cima -  A
+ *  entre 80 - 89   -  B
+ *  entre 70 - 79   -  C
+ *  entre 60 - 69   -  D
+ *  menor que  60   -  F
+ */
+/*
+let score = 100
+
+
+function getScore (score) {
+    let scoreA = score >= 90 && score <= 100
+    let scoreB = score >= 80 && score <= 89
+    let scoreC = score >= 70 && score <= 79
+    let scoreD = score >= 60 && score <= 69
+    let scoreF = score < 60 && score >= 0
+
+    let scoreFinal;
+
+
+    if (scoreA) {
+    scoreFinal = "A"
+  } else if (scoreB) {
+    scoreFinal = "B"
+  } else if (scoreC) {
+    scoreFinal = "C"
+  } else if (scoreD) {
+    scoreFinal = "D"
+  } else if (scoreF) {
+    scoreFinal = "F"
+  } else {
+    scoreFinal = "Nota inválida"
+  }
+
+   return scoreFinal
+
+}
+
+
+
+console.log(getScore(101))
+console.log(getScore(-1))
+console.log(getScore(0))
+console.log(getScore(1))
+console.log(getScore(45))
+console.log(getScore(60))
+console.log(getScore(61))
+console.log(getScore(75))
+console.log(getScore(85))
+console.log(getScore(95))
+
+
+ */
+
+/*  ### Sistema de gasto familair
+
+
+Crie um objrto que possuirá 2 propriedades, ambos do tipo array:
+* receitas: []
+* despesas: []
+
+
+Agora, crie uma função que irá calcular o total de receitas e despesas e irá msotrauma mensagem se a familia está com saldo positivo ou negativo, seguindo do valor do saldo
+
+
+
+*/
+
